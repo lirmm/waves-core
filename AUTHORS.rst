1e933fa1c162f8b26c2147286f80f9cf0e1cad77
Marc Chjakiachvili <marc.chakiachvili@lirmm.fr>
2017-12-19 10:11:53 +0100

    Changed AUTHORS

--- AUTHORS.rst
+++ AUTHORS.rst
@@ -3,4 +3,3 @@ Authors
 * Marc Chakiachvili (LIRMM - UMR 5506 CNRS / UM - France)
 * Vincent Lefort (LIRMM - UMR 5506 CNRS / UM - France)
 * Anne-Muriel Arigon Chiffoleau (LIRMM - UMR 5506 CNRS / UM - France)
-* Vincent Berry (LIRMM - UMR 5506 CNRS / UM - France)
--- waves/wcore/__init__.py
+++ waves/wcore/__init__.py
@@ -1,12 +1,12 @@
 """
 WAVES django components library
 """
-__version_detail__ = '1.1.5'
-__version__ = '1.1.5'
+__version_detail__ = '1.1.6'
+__version__ = '1.1.6'
 __author__ = 'Marc Chakiachvili, MAB Team'
 __licence__ = 'GPLv3'
 __copyright__ = "Copyright(C) 2015-2017, LIRMM - UM - CNRS"
-__db_version__ = "1.2"
+__db_version__ = "1.3"
 __name__ = "Waves Core package"
 
 default_app_config = 'waves.wcore.apps.WavesConfig'
