b462b50545e7e448538ace7e39b967c615693d8c
Marc Chakiachvili <marc.chakiachvili@gmail.com>
2018-06-03 18:42:32 +0100

    [Missing Dirs] - add default dirs

--- tests/data/sample/services/hello_world.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Working dir: $(pwd)"
-dir=$(pwd)
-sleep 5
-echo "Hello world" > ${dir}/hello_world_output.txt
-sleep 5
-echo "Follow " $1 >> ${dir}/hello_world_output.txt
-sleep 5
-echo "Last" $2 >> ${dir}/hello_world_output.txt
